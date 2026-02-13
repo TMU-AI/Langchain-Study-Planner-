120d3f61b27f9704ac376f78b2cf0c80adc56b7d
moonuel <co.manuel@hotmail.com>
2026-02-13 20:34:11 +0000

    feat: implemented high-temperature "Hello world" script

--- scripts/hello_openai.py
+++ scripts/hello_openai.py
@@ -1,1 +1,10 @@
-print("Hello world! I will be implemented using a real LLM call in a future update :)")
+from langchain.chat_models import init_chat_model
+import dotenv
+
+dotenv.load_dotenv()
+
+model = init_chat_model(model="gpt-5-nano", temperature=10)
+
+response = model.invoke("Say hello to the world! Be concise but funny")
+
+print(response.content)
